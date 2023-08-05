8222882aefb11d42ba6bd8e63eb5e5dc5ffbbd72
olga-matyasova <138147296+olga-matyasova@users.noreply.github.com>
2023-08-05 19:42:36 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import psycopg2
 from config import DATABASE_URL
 
+
 class Database:
     def __init__(self):
         self.conn = None
@@ -11,20 +12,21 @@ class Database:
         except (Exception, psycopg2.DatabaseError) as error:
             print(f'Ошибка подключения к базе данных: {error}')
 
-    def create_table(self, user_info_id, item_id):
+    def create_table(self):
         with self.conn:
             with self.conn.cursor() as cur:
-                cur.execute('CREATE TABLE IF NOT EXISTS results (user_info_id INT PRIMARY KEY, item_id INT PRIMARY KEY)')
+                cur.execute('CREATE TABLE IF NOT EXISTS results (user_id INT, matched_user_id INT)')
 
     def save_result(self, result):
         with self.conn:
             with self.conn.cursor() as cur:
-                cur.execute('INSERT INTO results (user_info_id, item_id) VALUES (%s, %s)', result)
+                cur.execute('INSERT INTO results (user_id, matched_user_id) VALUES (%s, %s)', result)
 
-    def check_user_id_database(self, user_info_id, item_id):
+    def check_user_in_database(self, user_id, matched_user_id):
         with self.conn:
             with self.conn.cursor() as cur:
-                cur.execute('SELECT * FROM results WHERE user_info_id = %s AND item_id = %s', (user_info_id, item_id))
+                cur.execute('SELECT * FROM results WHERE user_id = %s AND matched_user_id = %s',
+                            (user_id, matched_user_id))
                 return cur.fetchone() is not None
 
     def close(self):
